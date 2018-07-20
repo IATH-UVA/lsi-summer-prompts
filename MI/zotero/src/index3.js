dedc2921cd1d973dd315bf0292b209eb20b3c9e6
mi5ey <mi5ey@virginia.edu>
2018-07-20 10:18:52 -0400

    API, OCR, and mapping explorations

--- MI/zotero/src/index3.js
+++ MI/zotero/src/index3.js
@@ -4,6 +4,13 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
+var imageThumbURL = [];
+//array of item URL strings -> for bibliographic results
+var itemURL = [];
+
+//array of item bibliographic information -> for all results? start with bibliographic ones
+var bibInfo = [];
+
 const key = '4XchKcJeLMhVHlRj2J80nzAm' ;
 
 var params = {
@@ -13,7 +20,7 @@ var params = {
 			start: '0',
 			q: '',
 			qmode:'',
-			sort: 'title',
+			sort: 'title', 
 			//tag: '',
 			//itemType: 'book',
 			'api_key': key
@@ -33,10 +40,13 @@ window.onload=(()=>{
 
 	getReturns.then(items =>{
 	    var disAll = items.map(item=>item);
-	    console.log('here', disAll);
+	    //console.log('here', disAll);
 	    addCards(disAll);
+	    //addThumbnails(disAll);
 	    getMoreData(disAll);
 	});
+
+	//getBib(bibInfo);
     
     //console.log('here1', disAll);
 	//getSearchParams();
@@ -53,6 +63,26 @@ window.onload=(()=>{
 	console.log('Display: ',disAll);
 	//addCards(getReturns.then());
 */
+
+/*	console.log("what", imageThumbURL.length);
+	for(var m = 0; m<imageThumbURL.length;m++){
+		console.log('URL #', m, imageThumbURL[m]);
+	}
+
+	var footerElem = document.getElementById("footer");
+
+	//addCards(imageThumbURL);
+
+	//footerElem.innerHTML + "i am the footer";
+	//console.log('footer: ',footerElem.innerHTML);
+	var sorse = [];
+	var source = 'https://www.biodiversitylibrary.org/pagethumb/';
+
+	for(var m = 0; m <imageThumbURL.length; m++){
+		sorse.push(source + imageThumbURL[m]);
+		
+	}
+	console.log(sorse);*/
 
 });
 
@@ -90,7 +120,111 @@ const getSearchParams = (() =>{
 
 	)});
 
+/*
 
+Accessing Zotero:
+------------------------------------------------
+First we must make a call to the items stored in zotero. For that we developed a function 'basicCall' to handle 
+several types of calls, different parameters(stored in its own object), a limit of results to get back, and to handle any items to add.
+This function makes a call to the Zotero API, and stores the results displayed on 'cards' on the webpage.
+In order to handle the results from this call, a function 'basicReturns' was developed, that handles the specificity of
+the Zotero API and creates a result of data that can be further worked with to display on a webpage. 
+
+const basicCall=((type,params,limit,adds)=>{
+			var format= params.format, 
+			include= params.include,
+			v= params.v,
+			start = params.start,
+			q= params.q;
+		
+	var sample = `http://api.zotero.org/groups/2144277/items/top`
+	var paraObj = {
+		params: {format, include, v, limit, start, q, 'api_key':key}
+	}
+	
+	var initial; //hold data from initial call
+	var total;
+	var series =[]; //call and promises
+	var iterator = limit;
+
+	var basic = Axios[type](sample, paraObj) //get access to that location
+		.then(result=>{
+			total = result.headers['total-results'];
+			initial = result.data;
+			var i=1;
+				
+			while(start < +total){
+				start = i*iterator; //put into a series of array
+				series.push(Axios[type](sample, {params: {format, include, v, iterator, start, q, 'api_key':key} }));
+				i++;
+			}
+			 
+			return {
+				initial: initial,
+				series: series
+			}
+
+		})
+		.catch(console.log);
+		
+		return basic;
+});
+
+
+const basicReturns = (promObj=>{
+	
+	//compound all diff jsons getting back
+	var data = [];
+	
+	//mirror the way you structured the inital calls, handle the array first then crack open the promises
+	//getSeries is a promise....and promObj must be handled with basicCall before it is fullfilled
+	//
+	var getSeries = promObj.then(resObj=>{
+		//set empty data array to be inital array with first 25 results
+		data = resObj.initial;
+
+		//get series = series (now you can do a promise.all on getSeries)
+		//necessary to crack open in order to access the secondary layer of promises
+		return resObj.series;
+	})
+	.catch(console.log);
+	
+	//now can work with the array of arrays, and promise.all
+	//so once getSeries is fullfilled, which will happen when basicCall is fullfilled,
+	//allData will be fulfilled
+	var allData = Promise.all(getSeries).then(resSeries=>{
+		
+		//take every entry, i.e. every array of 25 results, & grab data
+		var res = resSeries.map(res=>res.data);
+
+		//add to OG data (25+849 objs)
+		data = data.concat(...res);
+
+		//one more to grab data from inital entries as well
+		data = data.map(res=>res.data);
+		return data;
+		
+	})
+	.catch(console.log);
+	return allData; //promise with master array of data
+	
+})
+
+
+Sorting Zotero Entries based on their source:
+------------------------------------------------
+There are over 800 Zotero Entries that we can now work with, however they are not all from the same source.
+Each is from a different online digital library, with its own individually structured data. So we divide and conquer: a method 
+is developed to organize the results into their own category, based on their source. Now we can begin to tackle 
+each individual source and their accompanying API.
+
+
+Individual Methods for Individual APIs:
+------------------------------------------------
+Due to the process of access an API being so varied for each one, we divided up the API acccessing
+into individual methods.
+
+*/
 
 
 const basicCall=((type,params,limit,adds)=>{
@@ -105,7 +239,7 @@ const basicCall=((type,params,limit,adds)=>{
 			//key = params.key;
 	
 		
-	//only looking at items in BHL folder
+	//only looking at items in BHL folder //collections/KXUA5LUF/
 	var sample = `http://api.zotero.org/groups/2144277/collections/KXUA5LUF/items/top`
 	var paraObj = {
 		params: {format, include, v, limit, start, q, 'api_key':key}
@@ -144,6 +278,7 @@ const basicCall=((type,params,limit,adds)=>{
 		return basic;
 
 		addCards(basic);
+		//addThumbnails(basic);
 
 });
 
@@ -171,6 +306,7 @@ const basicReturns = (promObj=>{
 		//console.log('second return: ', data);
 		return data;
 		addCards(data);
+		//addThumbnails(data);
 		
 	})
 	.catch(console.log);
@@ -179,6 +315,9 @@ const basicReturns = (promObj=>{
 
 	addCards(allData);
 	//console.log(addCards);
+	//addThumbnails(allData);
+
+
 	
 })
 
@@ -250,9 +389,7 @@ const getMoreData = (resultArr) =>{
 
 const getDataFromBHL=(arrBHL)=>{
 	//API information page --- https://biodivlib.wikispaces.com/Developer+Tools+and+API
-	console.log('BHL');
-
-	var imageThumbURL = [];
+	//console.log('BHL', arrBHL);
 
 	for(var i = 0; i < arrBHL.length; i++){
 		var j = arrBHL[i];
@@ -267,7 +404,7 @@ const getDataFromBHL=(arrBHL)=>{
 		var bhlSample = `https://www.biodiversitylibrary.org/api2/httpquery.ashx/search`;
 
 		if(j.includes('item')){
-			itemKey = '1';
+			//itemKey = '1';
 			// console.log(i,j, itemKey);
 			// console.log('item key: ', key);
 
@@ -283,14 +420,22 @@ const getDataFromBHL=(arrBHL)=>{
 				}
 			}
 			Axios.get(bhlSample, bhlParams1).then(res=>{
+				//console.log('testinn', res.data);
+				//console.log('get call for item: ', res.data.Result.ThumbnailPageID);
+				imageThumbURL.push(res.data.Result.ItemThumbUrl);
+				//itemURL.push(res.data.Result.ItemURL);
+				//console.log('ULRS ', imageThumbURL);
+				//format(res.data.Result);
+				console.log('res.data: ', res.data);
+				addThumbnails(imageThumbURL);
+				//addThumbnails(res.data.Result);
 
-				console.log('get call for item: ', res.data.Result.ThumbnailPageID);
-				imageThumbURL.push(res.data.Result.ThumbnailPageID);
+				//console.log("itemURL array:", itemURL);
 
 			}).catch(console.log);
 		}
 
-		if(j.includes('bibliography')){
+		else if(j.includes('bibliography')){
 			bibKey = '0';
 
 			var bhlParams2 = {
@@ -299,7 +444,8 @@ const getDataFromBHL=(arrBHL)=>{
 					'apikey': bhlKey,
 					op: 'GetTitleMetadata',
 					titleid: key,
-					items: 't'
+					items: 't',
+					ocr: 't',
 				}
 			}
 			Axios.get(bhlSample, bhlParams2).then(res1=>{
@@ -307,31 +453,46 @@ const getDataFromBHL=(arrBHL)=>{
 				var t = res1.data.Result.Items.length;
 
 				for(var a = 0; a < t; a++){
-					imageThumbURL.push(res1.data.Result.Items[a].ThumbnailPageID);
+					imageThumbURL.push(res1.data.Result.Items[a].ItemThumbUrl);
+					//itemURL.push(res1.data.Result.Items[a].ItemURL);
+					//format(res1.data.Result.Items[a]);
+					//console.log("itemURL array:", itemURL);
+					addThumbnails(res1.data.Result.Items[a]);
+					console.log(res1.data, "res1 DATA");
 				}
 				
+			}).catch(console.log); 
+
+			var bhlParams3 = {
+				params: {
+					format: 'json',
+					'apikey': bhlKey,
+					op: 'GetTitleBibTex',
+					titleid: key,
+					items: 't',
+					ocr: 't',
+				}
+			}
+
+			Axios.get(bhlSample, bhlParams3).then(res2=> {
+				
+				bibInfo.push(res2.data.Result);
+				//console.log("bibInfo: ", bibInfo);
+				//the resulting information can be downloaded and opened as a new item in zotero
+				//this has a correct -- 'item' url
+				//getBib(bibInfo);
+
 			}).catch(console.log);
 		}
+
+		else{
+			console.log("BHL call not working");
+		}
+
 	}
-
-	console.log("imageThumbURLs",imageThumbURL);
-
-	//append image to footer
-	document.querySelector('#footer').append(imageThumbURL[0]);
-
 
 	return getDataFromBHL;
 }
-
-//------------------------------Viewing Results from BHL-----------------------------------
-//use bootstrap images https://www.w3schools.com/bootstrap/bootstrap_images.asp
-//	1) get the thumbnail URL
-//	2) use it as the 'src' for a responsive embed
-//		-> https://www.w3schools.com/bootstrap/tryit.asp?filename=trybs_responsive_embed&stacked=h
-//	3) create images in a similar vain to how the cards were created
-//	4) add the images to the footer bar
-//-----------------------------------------------------------------------------------------
-
 
 const getDataFromHathitrust=(arrHathi)=>{
 	//API information page --- https://libraryofcongress.github.io/data-exploration/#requests
@@ -344,35 +505,6 @@ const getDataFromHathitrust=(arrHathi)=>{
 const getDataFromLOC=(arrLOC)=>{
 	console.log('LOC');
 
-	//request a specific item ---- /item/{identifier}/? 
-	//for example --- https://www.loc.gov/item/ggb2006012811/?fo=json
-
-	//check the format of the existing urls
-	//get item id from the existing array of url
-	//map through the id information and grab data from api request
-
-/*  for(var i=0; i<LOCArr.length; i++){
-	var LocID = LOCArr[i].substr(25, 8);
-	//another ways: str.slice(beginIndex[, endIndex]), str.split([separator[, limit]])
-}
-    
-    const grabFormat = ((subject, format)=>{
-	var sample = `https://www.loc.gov/`
-	var paraObj = {
-		params: {
-			fo:'json',
-			c:'50',
-			at:item,resources,reproductions
-            //?? --- at!=more_like_this
-            sb:date_desc
-            sb:shelf_id
-			api_key: kDP,
-		}
-	}
-	return Axios.get(sample,paraObj);
-	});
-*/
-	
 }
 
 
@@ -401,19 +533,12 @@ const getHathiPage = (itemOrigRec) =>{ //pass in entries' original record which 
 
 }
 
-
-
-
 const arrStr = (item)=>{
 	var resEntry = '';
 	Array.isArray(item)? resEntry=item[0] : resEntry=item;
 
 	return resEntry;
 }
-
-
-
-
 
 const simpEntry = (itemObj)=>{
 	return {
@@ -426,65 +551,106 @@ const simpEntry = (itemObj)=>{
 	}
 }
 
-
-
-
 const addCards = (arr) => {
+	console.log("addCards input here: ", arr);
 
 	if (!document.querySelector('#cards')) {
 			var cards = document.createElement('div');
 			cards.id = "cards";
 			cards.style.display = 'flex';
 			cards.style.flexWrap = 'wrap';
-			cards.style.height = '80vh';
+			cards.style.height = '60vh';
 			cards.style.overflow ='auto';
 	}
 
 	arr.forEach(entry=>{
 
-		//4) add the html with inserted js to create the cards here
-
 		var card = '';
 
 		var card =` <div class="card" style="width: 17rem; margin-right: 1rem;margin-top: 2rem;">
 		  <div class="card-header">
-		    ItemType: ${entry.itemType}
+		    ${entry.title}
 		  </div>
 		    <div class="card-body">
-		    <a href= ${entry.url} target="_blank"><h5 class="card-title"> ${entry.title}</h5></a>
 		      <ul>
 		      	<li>place: ${entry.place}</li>
 		      	<li>author: ${entry.creators[0].lastName}, ${entry.creators[0].firstName}</li>
 		      	<li>date: ${entry.date}</li>
 		      	<li>tag: ${entry.tags}</li>
+		      	<li> <a href= ${entry.url} target="_blank">see on BHL</a></li>
+
 		      </ul>
 		    </div>
   		</div>`
-/*
-var card =` <div class="card" style="width: 17rem; margin-right: 1rem;margin-top: 2rem;">
-		  <div class="card-header">
-		    ItemType: ${entry.data.data.itemType}
-		  </div>
-	    <div class="card-body">
-	    <a href=${entry.data.data.url} target="_blank"><h5 class="card-title">${entry.data.title}</h5></a>
-	      <ul>
-	      	<li>place:${entry.data.data.place}</li>
-	      	<li>author:${entry.data.data.creators}</li>
-	      	<li>date:${entry.data.data.date}</li>
-	      </ul>
-	    </div>
-  </div>`
-*/
-
-		cards.innerHTML += card ;
+		cards.innerHTML += card;
 	})
 
 	document.querySelector('.col-10').append(cards);
 
 }
 
+const format = (itemObj)=>{
 
-//other ideas:
+	//console.log("formating", itemObj); 
 
-				//aggregate by decade then medium, decade year then author, decade then source
-				//radio buttons to that effect
+	return {
+		id: itemObj.ItemID,
+		pages: (itemObj.Pages)? itemObj.Pages.length: null,
+		tID: itemObj.ThumbnailPageID,
+		src: itemObj.Source,
+		//abstract: (itemObj.data.abstractNote) ? arrStr(itemObj.data.abstractNote) : null,
+		ano: itemObj.Year,
+		link: itemObj.ItemURL,
+		image: itemObj.ItemThumbURL,
+	}
+}
+
+const getBib = (arr) =>{
+	for (var i = 0; i < arr.length; i++){
+		return arr[i];
+	}
+}
+
+const addThumbnails = (arr) => {
+	console.log("addThumbnails input here", arr);
+
+	var thumbID;
+	
+	if(!document.querySelector('#footer')) {
+		var nails = document.createElement('div');
+		nails.id = "footer";
+		nails.style.display = 'flex';
+		nails.style.flexWrap = 'wrap';
+		nails.style.height = '20vh';
+		nails.style.overflow = 'auto';
+	}
+
+	arr.forEach(entry=>{
+		var thumb = '';
+		var k = entry.lastIndexOf('/');
+		thumbID = entry.substring(k+1,(entry.length));
+
+		var thumb  = `
+		<div class = "col" >
+			<div class="thumbnail">  
+				<div class = "caption">
+					<div class="dropdown">
+					    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown">${thumbID}
+					    <span class="caret"></span></button>
+					    <ul class="dropdown-menu">
+					      <li class = "active"><a href="${entry}">Full Size</a></li>
+					      <li class = "disabled"><a href="#">Item</a></li>
+					      <li class="divider"></li>
+					      <li class = "disabled"><a href="#" download>Download</a></li>
+					    </ul>
+				  	</div>
+				</div>
+				<img src="${entry}" class="img-thumbnail" width = "50" alt="result thumbnail here"> 
+			</div>
+		</div>`
+
+		nails.innerHTML += thumb;
+	})
+	document.querySelector('.row-med').append(nails);
+
+}
